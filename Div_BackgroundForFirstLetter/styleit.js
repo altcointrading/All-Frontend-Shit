76d547009fb5d1596e53cbb2a58403a6f8c420b3
I won't tell <mthjn@users.noreply.github.com>
2015-07-21 16:13:35 +0200

    Update styleit.js

--- Div_BackgroundForFirstLetter/styleit.js
+++ Div_BackgroundForFirstLetter/styleit.js
@@ -1,12 +1,20 @@
-//written for a long A - Z list
+//---written for a long A - Z list---//
 
-var link=document.getElementsByTagName("li");
+//in footer on loaded dom!
+window.addEventListener("load", function() {
+      doBackground( "li" );
+});
 
-for(var i=0;i<link.length;i++) {
-  var str = link[i].innerHTML;
-  var n = str.charAt(0);
-  //color bg at every other letter of abc
-  if ( n == "A" || n == "C" || n == "E"|| n == "G"|| n == "C"|| n == "I"|| n == "K"|| n == "M"|| n == "O"|| n == "Q"|| n == "S"|| n == "U"|| n == "W"|| n == "Y") {
-    link[i].style.background="#eff1f7";
+function doBackground( passedtag )
+{
+  var link=document.getElementsByTagName( passedtag );
+
+  for(var i=0;i<link.length;i++) {
+    var str = link[i].innerHTML;
+    var n = str.charAt(0);
+    //color bg at every other letter of abc
+    if ( n == "A" || n == "C" || n == "E"|| n == "G"|| n == "C"|| n == "I"|| n == "K"|| n == "M"|| n == "O"|| n == "Q"|| n == "S"|| n == "U"|| n == "W"|| n == "Y") {
+      link[i].style.background="#eff1f7";
+     }
   }
 }
